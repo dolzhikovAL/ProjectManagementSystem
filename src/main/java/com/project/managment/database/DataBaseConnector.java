60f6ddeb1afff2f75e610cbefcb3241a203d3f85
Dolzhykov aleksander <dolzhikov.al@yandex.ua>
2020-10-09 10:24:38 +0300

    Initial commit, add DataBaseConnector,add DML.sql,DDL.sql

---
+++ src/main/java/com/project/managment/database/DataBaseConnector.java
@@ -0,0 +1,43 @@
+package com.project.managment.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class DataBaseConnector {
+    private static final HikariDataSource ds;
+
+
+    static {
+        HikariConfig config = new HikariConfig();
+        final Properties properties = new Properties();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            try (InputStream inputStream = classLoader.getResourceAsStream("application.properties")
+            ) {
+                properties.load(inputStream);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error load application properties");
+        }
+
+        config.setJdbcUrl(properties.getProperty("jdbc.url"));
+        config.setUsername("jdbc.username");
+        config.setPassword("jdbc.password");
+        ds= new HikariDataSource(config);
+        ds.setMaximumPoolSize(10);
+    }
+}
+
+
+
+
+
+
+
---
+++ src/main/java/com/project/managment/domain/Developer.java
@@ -0,0 +1,4 @@
+package com.project.managment.domain;
+
+public class Developer {
+}
